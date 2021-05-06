cc4c6d838711ce060dcc8a20dce4cd63c8225563
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-05-06 14:24:16 -0500

    Added delete message on product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -77,7 +77,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json( {message: `You have successfully deleted this category.`});;
+    res.status(200).json( {message: `You have successfully deleted this category.`});
 
   } catch (err) {
     res.status(500).json(err)
